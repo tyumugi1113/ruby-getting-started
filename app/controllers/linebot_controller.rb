5b3cce5fa62a74938bb1784f23b41d6d421a2237
Your Name <tyuumugi@gmail.com>
2019-12-15 02:07:14 +0000

    aa

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -12,14 +12,14 @@ class LinebotController < ApplicationController
      end
      
      def callback
-       body = request.body.read
+       body = request.body.read //リクエスト(文字)を読み込む
    
        signature = request.env["HTTP_X_LINE_SIGNATURE"]
        unless client.validate_signature(body, signature)
          error 400 do "Bad Request" end
        end
    
-       events = client.parse_events_from(body)
+       events = client.parse_events_from(body) //イベントをbodyから取り出す
    
        events.each { |event|
          case event
@@ -28,8 +28,10 @@ class LinebotController < ApplicationController
            when Line::Bot::Event::MessageType::Text
              message = {
                type: "text",
-               text: event.message["text"] + "!"
+               text: hello(event.message["text"])
              }
+             
+             
              client.reply_message(event["replyToken"], message)
            when Line::Bot::Event::MessageType::Location
              message = {
@@ -46,4 +48,10 @@ class LinebotController < ApplicationController
    
        head :ok
      end
+     
+     def hello(text)
+         return text + "!!!!!!!!!!!!!1"
+     end
+     
+     
  end
